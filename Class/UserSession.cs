380d616ea1362e9dfcf7532025a244bf79ef058f
ThuyHa180503 <24a4040617@bav.edu.vn>
2024-06-06 04:00:18 +0700

    Add files via upload

---
+++ Class/UserSession.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhom7_Project_QLPM.Class
+{
+    internal class UserSession
+    {
+        public static string AccountId { get; private set; }
+        public static string ChucVu { get; private set; }
+
+        public static void StartSession(string accountId, string chucVu)
+        {
+            AccountId = accountId;
+            ChucVu = chucVu;
+        }
+
+        public static void EndSession()
+        {
+            AccountId = null;
+            ChucVu = null;
+        }
+
+        public static bool IsLoggedIn()
+        {
+            return !string.IsNullOrEmpty(AccountId);
+        }
+    }
+}
